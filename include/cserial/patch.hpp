19766b6d5306e731ccfed768c0dededebbb14545
Christian Kurz <chrikuvellberg@gmail.com>
2021-07-31 19:13:08 +0200

    object patch

--- include/cserial/patch.hpp
+++ include/cserial/patch.hpp
@@ -14,21 +14,18 @@ namespace cserial {
     };
     template <typename self_type> struct patch_value;
     template <typename self_type> struct patch_value_struct {
-      static void apply(self_type& value, patch& p) {
-        std::string name = std::get<std::string>(p.key.at(p.keypos));
+      static void apply(self_type& value, nlohmann::json p) {
         serial<self_type>::iterate(
             &value, [&]<typename current_field>(self_type*, current_field*) constexpr {
-              std::cout << current_field::name() << ": " << name << std::endl;
-              if (current_field::name() == name) {
-                p.keypos++;
-                patch_value<typename current_field::value_type>::apply(value.*current_field::member_pointer(), p);
+              if (!p[std::string(current_field::name())].is_null()) {
+                patch_value<typename current_field::value_type>::apply(value.*current_field::member_pointer(), p[std::string(current_field::name())]);
               }
             });
       }
     };
     template <typename self_type> struct patch_value_convert {
       using serial_type = typename serial<self_type>::serial_type;
-      static void apply(self_type& value, patch& p) {
+      static void apply(self_type& value, nlohmann::json p) {
         serial_type v2;
         serial<self_type>::convert(value, v2);
         patch_value<serial_type>::apply(v2, p);
@@ -38,7 +35,7 @@ namespace cserial {
     template <typename self_type>
     struct patch_value : std::conditional_t<std::is_base_of_v<available_converter, serial<self_type>>, patch_value_convert<self_type>, patch_value_struct<self_type>> {};
     template <typename int_type> struct patch_value_simple {
-      static void apply(int_type& value, patch& p) { value = p.value.get<int_type>(); }
+      static void apply(int_type& value, nlohmann::json p) { value = p.get<int_type>(); }
     };
     template <> struct patch_value<uint64_t> : patch_value_simple<uint64_t> {};
     template <> struct patch_value<int64_t> : patch_value_simple<int64_t> {};
@@ -53,70 +50,48 @@ namespace cserial {
     template <> struct patch_value<bool> : patch_value_simple<bool> {};
     template <> struct patch_value<std::string> : patch_value_simple<std::string> {};
     template <> struct patch_value<std::monostate> {
-      static void apply(std::monostate& value, patch& p) {}
+      static void apply(std::monostate& value, nlohmann::json p) {}
     };
     template <typename... types> struct patch_value<std::variant<types...>> {
-      template <size_t i, typename current_type, typename... other> static void apply_cond(std::variant<types...>& value, patch& p, size_t name) {
-        if (i == name) {
+      template <size_t i, typename current_type, typename... other> static void apply_cond(std::variant<types...>& value, nlohmann::json p) {
+        if (p.contains(std::to_string(i))) {
           value = std::variant<types...>(std::in_place_index<i>);
-          patch_value<current_type>::apply(std::get<i>(value), p);
+          patch_value<current_type>::apply(std::get<i>(value), p[std::to_string(i)]);
         } else if constexpr (sizeof...(other) > 0)
-          apply_cond<i + 1, other...>(value, p, name);
+          apply_cond<i + 1, other...>(value, p);
       }
-      static void apply(std::variant<types...>& value, patch& p) {
-        size_t name = std::get<size_t>(p.key.at(p.keypos));
-        p.keypos++;
-        apply_cond<0, types...>(value, p, name);
-      }
+      static void apply(std::variant<types...>& value, nlohmann::json p) { apply_cond<0, types...>(value, p); }
     };
     template <typename subtype> struct patch_value<std::unordered_map<std::string, subtype>> {
-      static void apply(std::unordered_map<std::string, subtype>& value, patch& p) {
-        std::string name = std::get<std::string>(p.key.at(p.keypos));
-        p.keypos++;
-        if ((p.keypos == p.key.size()) && (p.value.is_null())) {
-          value.erase(name);
-        } else
-          patch_value<subtype>::apply(value[name], p);
+      static void apply(std::unordered_map<std::string, subtype>& value, nlohmann::json p) {
+        for (const auto& [key, val] : p.items()) {
+          if (value.is_null()) {
+            value.erase(key);
+          } else {
+            patch_value<subtype>::apply(value[key], val);
+          }
+        }
       }
     };
     template <typename subtype> struct patch_value<std::vector<subtype>> {
-      static void apply(std::vector<subtype>& value, patch& p) {
-        size_t name = std::get<size_t>(p.key.at(p.keypos));
-        p.keypos++;
-        if ((p.keypos == p.key.size()) && (p.value.is_null())) {
-          value.resize(name);
-          return;
-        } else if (value.size() <= name)
-          value.resize(name + 1);
-        patch_value<subtype>::apply(value.at(name), p);
+      static void apply(std::vector<subtype>& value, nlohmann::json p) {
+        for (const auto& [key, val] : p.items()) {
+          size_t name;
+          if (auto [p, ec] = std::from_chars(key.data(), key.data() + key.size(), name); (ec == std::errc()) && (p[0] == '\0')) {
+            if (value.is_null()) {
+              value.resize(name);
+            } else {
+              if (value.size() <= name)
+                value.resize(name + 1);
+              patch_value<subtype>::apply(value.at(name), p);
+            }
+          }
+        }
       }
     };
-    std::vector<patch> ensure_split(patch in) {
-      if (in.value.is_object()) {
-        std::vector<patch> ret;
-        for (const auto& [key, value] : in.value.items()) {
-          patch np = in;
-          size_t result;
-          if (auto [p, ec] = std::from_chars(key.data(), key.data() + key.size(), result); (ec == std::errc()) && (p[0] == '\0')) {
-            np.key.push_back(result);
-          } else
-            np.key.push_back(key);
-          np.value = value;
-          for (auto sp : ensure_split(np))
-            ret.push_back(sp);
-        }
-        return ret;
-      } else {
-        return std::vector<patch>{in};
-      }
-    }
     template <typename self_type> void apply(self_type& o, nlohmann::json object) {
       if (object.is_object()) {
-        patch root_patch;
-        root_patch.value = object;
-        auto p = ensure_split(root_patch);
-        for (auto& p : p)
-          patch_value<self_type>::apply(o, p);
+        patch_value<self_type>::apply(o, object);
       } else
         for (const auto& so : object)
           apply<self_type>(o, so);
