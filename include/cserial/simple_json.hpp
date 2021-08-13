7b0dbf73e8d48e3e7172e0c6f6829ff7f90921ef
Christian Kurz <chrikuvellberg@gmail.com>
2021-08-13 08:02:56 +0200

    raw

--- include/cserial/simple_json.hpp
+++ include/cserial/simple_json.hpp
@@ -12,7 +12,9 @@
 #include <vector>
 
 namespace cserial {
-  struct simple_json {};
+  struct simple_json {
+    struct all {};
+  };
 } // namespace cserial
 
 namespace nlohmann {
@@ -20,17 +22,23 @@ namespace nlohmann {
     static_assert(!std::is_base_of_v<cserial::available_converter, cserial::serial<self_type>>);
     static void to_json(json& j, const self_type& value) {
       j = json::object();
-      cserial::serial<self_type>::iterate(
-          const_cast<self_type*>(&value), [&]<typename current_field>(self_type*, current_field*) constexpr {
-            j[std::string(current_field::name())] = json(value.*current_field::member_pointer());
-          });
+      cserial::serial<self_type>::iterate(const_cast<self_type*>(&value), [&]<typename current_field>(self_type*, current_field*) {
+        if constexpr (!current_field::template has_parameter<cserial::simple_json::all>())
+          j[std::string(current_field::name())] = json(value.*current_field::member_pointer());
+      });
     }
 
     static void from_json(const json& j, self_type& value) {
-      cserial::serial<self_type>::iterate(
-          const_cast<self_type*>(&value), [&]<typename current_field>(self_type*, current_field*) constexpr {
+      cserial::serial<self_type>::iterate(const_cast<self_type*>(&value), [&]<typename current_field>(self_type*, current_field*) {
+        if constexpr (!current_field::template has_parameter<cserial::simple_json::all>()) {
+          if (j.contains(std::string(current_field::name())))
             value.*current_field::member_pointer() = j[std::string(current_field::name())].get<typename current_field::value_type>();
-          });
+          else
+            value.*current_field::member_pointer() = nlohmann::json().get<typename current_field::value_type>();
+        } else {
+          value.*current_field::member_pointer() = j.get<typename current_field::value_type>();
+        }
+      });
     }
   };
   template <typename T> struct adl_serializer<std::optional<T>> {
@@ -49,5 +57,9 @@ namespace nlohmann {
         opt = j.get<T>();
       }
     }
+  };
+  template <> struct adl_serializer<nlohmann::json> {
+    static void to_json(json& j, const json& opt) { j = opt; }
+    static void from_json(const json& j, json& opt) { opt = j; }
   };
 } // namespace nlohmann
